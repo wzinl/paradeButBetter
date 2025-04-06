c3fcad0a20c16a2b5eee27ed873941f8ffce9a51
wzinl <waizinln3@gmail.com>
2025-04-06 23:44:56 +0800

    changed printf to println

--- src/main/java/main/helpers/ui/DisplayEffects.java
+++ src/main/java/main/helpers/ui/DisplayEffects.java
@@ -3,30 +3,30 @@ package main.helpers.ui;
 
 public class DisplayEffects {
 
-    // Text Colouring
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_MAGENTA = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_BRIGHT_WHITE = "\u001B[97m";
+// Text Colouring
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_RED = "\u001B[31m";
+public static final String ANSI_GREEN = "\u001B[32m";
+public static final String ANSI_YELLOW = "\u001B[33m";
+public static final String ANSI_BLUE = "\u001B[34m";
+public static final String ANSI_MAGENTA = "\u001B[35m"; // Magenta
+public static final String ANSI_CYAN = "\u001B[36m";
+public static final String ANSI_PURPLE = "\u001B[95m"; // Distinct purple
+public static final String ANSI_BRIGHT_WHITE = "\u001B[97m";
 
-    // Background Colouring
-    public static final String MAGENTA_BG = "\u001B[45m";
-    public static final String RED_BG = "\u001B[41m";
-    public static final String BLUE_BG = "\u001B[44m";
-    public static final String LBLUE_BG = "\u001B[104m";
-    public static final String YELLOW_BG = "\u001B[43m";
-    public static final String GREEN_BG = "\u001B[42m";
-    public static final String PURPLE_BG = "\u001B[45m";
+// Background Colouring
+public static final String MAGENTA_BG = "\u001B[45m";
+public static final String RED_BG = "\u001B[41m";
+public static final String BLUE_BG = "\u001B[44m";
+public static final String LBLUE_BG = "\u001B[104m"; 
+public static final String YELLOW_BG = "\u001B[43m";
+public static final String GREEN_BG = "\u001B[42m";
+public static final String PURPLE_BG = "\u001B[105m";
 
-    // Text Effects
-    public static final String BOLD = "\u001B[1m";
-    public static final String ANSI_UNDERLINE = "\u001B[4m";
-    public static final int BLINK_COUNT = 20;
+// Text Effects
+public static final String BOLD = "\u001B[1m";
+public static final String ANSI_UNDERLINE = "\u001B[4m";
+public static final int BLINK_COUNT = 20;
 
 
     public static void typeWriter(String text, int delay) throws InterruptedException {
